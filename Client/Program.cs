0c6460b15146a8f851f7caaf2a5807090a12595f
Sam Bosell <sam.bosell@ask.vet>
2022-02-16 09:03:20 -0500

    Blazorise starter init repo
    Setup blazorise WASM to test out some components with a .net hosted

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using BlazoriseTest2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services
+      .AddBlazorise(options =>
+      {
+          options.ChangeTextOnKeyPress = true;
+      })
+      .AddBootstrapProviders()
+      .AddFontAwesomeIcons();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
